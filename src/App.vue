d61c170f39388b56e99bf2e1244bbfabda1834ce
ykycoder <649074096@qq.com>
2025-08-15 00:10:03 +0800

    进行中文国际化配置

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import BasicLayout from './layouts/BasicLayout.vue'
-
 import { healthCheck } from '@/api/healthController.ts'
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import { ConfigProvider } from 'ant-design-vue';
+
+dayjs.locale('zh-cn');
 
 healthCheck().then((res) => {
   console.log(res)
 })
-
 </script>
 
 <template>
-  <BasicLayout />
+  <a-config-provider :locale="zhCN">
+    <BasicLayout />
+  </a-config-provider>
 </template>
 
 <style scoped>
